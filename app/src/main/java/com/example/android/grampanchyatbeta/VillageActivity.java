90a32c941b11028bbb89d372097b742d9a341a56
Siddharth Singh <siddharths_bt2k16@dtu.ac.in>
2018-03-12 19:26:11 +0530

    Initial build

---
+++ app/src/main/java/com/example/android/grampanchyatbeta/VillageActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.grampanchyatbeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VillageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_village);
+    }
+}
